966cb0fb38391c64156340a0cefcfc290f62ecf2
Luciano Mattoli <luchileigh8@gmail.com>
2023-09-04 03:43:20 +0000

    Created manual entry for the 'ver' command

--- source/os/shell.ts
+++ source/os/shell.ts
@@ -234,6 +234,9 @@ module TSOS {
                     case "help":
                         _StdOut.putText("Help displays a list of (hopefully) valid commands.");
                         break;
+                    case "ver":
+                        _StdOut.putText("This displays the name and the current version of the os.");
+                        break;
                     // TODO: Make descriptive MANual page entries for the the rest of the shell commands here.
                     default:
                         _StdOut.putText("No manual entry for " + args[0] + ".");
