139099348d4bf5a15659653b645107b65aa6ea90
Luciano Mattoli <luchileigh8@gmail.com>
2023-09-18 02:45:53 +0000

    Created memory, currently freezes OS upon boot

---
+++ distrib/host/memory.js
@@ -0,0 +1,26 @@
+var TSOS;
+(function (TSOS) {
+    class Memory {
+        memArray;
+        constructor(memArray = new Array(0xFF)) {
+            this.memArray = memArray;
+        }
+        // Fills the main memory with empty values upon initialization
+        init() {
+            this.memArray.fill(0x00);
+        }
+        // Below commands are used by memoryAccessor to work with 
+        // Gets the value of a given address
+        getAddr(addr) {
+            return this.memArray[addr];
+        }
+        // Sets the value of a given address
+        setAddr(addr, value) {
+            if (addr <= 0xFF) {
+                this.memArray[addr] = value;
+            }
+        }
+    }
+    TSOS.Memory = Memory;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=memory.js.map
---
+++ source/host/memory.ts
@@ -0,0 +1,26 @@
+module TSOS {
+
+    export class Memory {
+
+        constructor(public memArray = new Array<number>(0xFF)) {
+        }
+
+        // Fills the main memory with empty values upon initialization
+        public init(): void {
+            this.memArray.fill(0x00);
+        }
+
+        // Below commands are used by memoryAccessor to work with 
+        // Gets the value of a given address
+        public getAddr(addr: number): number {
+            return this.memArray[addr];
+        } 
+
+        // Sets the value of a given address
+        public setAddr(addr: number, value: number) {
+            if (addr <= 0xFF) {
+                this.memArray[addr] = value;
+            }
+        }
+    }
+}
