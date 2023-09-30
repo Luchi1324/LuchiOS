199bb84baf8bbf05a4c3d0e0547907159c991ed1
Luciano Mattoli <luchileigh8@gmail.com>
2023-09-30 23:13:23 +0000

    Updated some comments

--- distrib/host/memoryAccessor.js
+++ distrib/host/memoryAccessor.js
@@ -3,9 +3,13 @@ var TSOS;
     class MemoryAccessor {
         constructor() {
         }
+        // Reads memory from a location based on it's relative PCB base register
+        // Not particaruly useful now, but just future proofing when we need to load more than one program. 
+        // It should make loops easier (i.e. just using one loop that starts at 0x00 for everything instead of setting a new loop based on the PCB's base register)
         readMem(pcb, addr) {
             return _Memory.getAddr(pcb.baseReg + addr);
         }
+        // Writes memory to a location based on it's relative PCB base register
         writeMem(pcb, addr, value) {
             _Memory.setAddr(pcb.baseReg + addr, value);
         }
--- source/host/memoryAccessor.ts
+++ source/host/memoryAccessor.ts
@@ -4,10 +4,14 @@ module TSOS {
 
         }
 
+        // Reads memory from a location based on it's relative PCB base register
+        // Not particaruly useful now, but just future proofing when we need to load more than one program. 
+        // It should make loops easier (i.e. just using one loop that starts at 0x00 for everything instead of setting a new loop based on the PCB's base register)
         public readMem(pcb: TSOS.ProcessControlBlock, addr: number) {
             return _Memory.getAddr(pcb.baseReg + addr);
         }
 
+        // Writes memory to a location based on it's relative PCB base register
         public writeMem(pcb: TSOS.ProcessControlBlock, addr: number, value: number) {
             _Memory.setAddr(pcb.baseReg + addr, value);
         }
