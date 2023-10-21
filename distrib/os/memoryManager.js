80083b0f2765316fa8ca22668b712e341f5f45eb
Luchi1324 <luchileigh8@gmail.com>
2023-10-21 12:22:13 -0400

    Seperated allocateMem into two functions

--- distrib/os/memoryManager.js
+++ distrib/os/memoryManager.js
@@ -3,8 +3,10 @@ var TSOS;
     class MemoryManager {
         pcbArr;
         segMap;
+        currSeg;
         constructor() {
             this.pcbArr = [];
+            this.currSeg = 0;
             this.segMap = {
                 0x000: false,
                 0x100: false,
@@ -16,48 +18,49 @@ var TSOS;
             // Creates new PCB object ...
             let pcb = new TSOS.ProcessControlBlock();
             // ... and if the memory can be allocated
-            if (this.allocateMem(pcb, program) === true) {
+            if (this.canBeAllocated(program) === true) {
                 pcb.createPCB();
                 this.pcbArr.push(pcb);
+                this.allocateMem(pcb, program);
                 return true;
             }
             else {
                 return false;
             }
         }
-        // TODO: Fix allocateMem not handing all segments being allocated already properly
-        allocateMem(pcb, program) {
+        canBeAllocated(program) {
             for (let i = 0x000; i <= 0x300; i += 0x100) {
-                // First we check if the memory segment has been allocated already ...
-                if (this.segMap[i] === true) {
-                    continue;
-                    // ... and if is not allocated then we set the PCB's base and limit registers ...
-                }
-                else if (this.segMap[i] === false) {
-                    this.segMap[i] = true;
-                    pcb.baseReg = i;
-                    pcb.limitReg = pcb.baseReg + 0xFF;
-                    break;
-                    // ... otherwise they're all allocated and we exit the function before anything is written.
+                if (this.segMap[i] === false && program.length <= 0xFF) {
+                    this.currSeg = i;
+                    return true;
                 }
                 else if (this.segMap[i] === 'allAllocated') {
                     return false;
                 }
             }
+        }
+        allocateMem(pcb, program) {
+            this.segMap[this.currSeg] = true;
+            pcb.baseReg = this.currSeg;
+            pcb.limitReg = pcb.baseReg + 0xFF;
             // Now we actually write the program to memory
             for (let i = 0; i < program.length; i++) {
                 _MemoryAccessor.writeMem(pcb, i, program[i]);
             }
-            return true;
         }
+        // Clears a segment of memory associated with a PCB
+        clearMemSeg(pcb) {
+            for (let i = pcb.baseReg; i < pcb.limitReg; i++) {
+                _MemoryAccessor.writeMem(pcb, i, 0x00);
+            }
+            // Marks segment as unallocated
+            this.segMap[pcb.baseReg] = false;
+            TSOS.Devices.hostUpdateMemDisplay();
+        }
+        // Clears all of the memory, and their associated PCBs by clearing each segment
         clearMem() {
-            // Create a temp PCB to use the MA, then we undo the currentPID increment so it is written over when we actually create another PCB
-            let tempPcb = new TSOS.ProcessControlBlock();
-            tempPcb.createPCB();
-            TSOS.ProcessControlBlock.currentPID -= 1;
-            tempPcb.limitReg = 0x301;
-            for (let i = 0; i < 0x300; i++) {
-                _MemoryAccessor.writeMem(tempPcb, i, 0x000);
+            for (let i in this.pcbArr) {
+                this.clearMemSeg(this.pcbArr[i]);
             }
             TSOS.Devices.hostUpdateMemDisplay();
         }
--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -3,9 +3,11 @@ module TSOS {
     export class MemoryManager {
         public pcbArr: ProcessControlBlock[];
         public segMap: Object;
+        public currSeg: number;
 
         constructor() {
             this.pcbArr = [];
+            this.currSeg = 0;
             this.segMap = {
                 0x000: false,
                 0x100: false,
@@ -18,9 +20,10 @@ module TSOS {
             // Creates new PCB object ...
             let pcb = new ProcessControlBlock();
             // ... and if the memory can be allocated
-            if (this.allocateMem(pcb, program) === true) {
+            if (this.canBeAllocated(program) === true) {
                 pcb.createPCB();
                 this.pcbArr.push(pcb);
+                this.allocateMem(pcb, program)
                 return true;
             } else {
                 return false;
@@ -28,39 +31,42 @@ module TSOS {
 
         }
 
-        // TODO: Fix allocateMem not handing all segments being allocated already properly
-        public allocateMem(pcb: TSOS.ProcessControlBlock, program: number[]): boolean {
+        public canBeAllocated(program: number[]): boolean {
             for (let i = 0x000; i <= 0x300; i += 0x100) {
-                // First we check if the memory segment has been allocated already ...
-                if (this.segMap[i] === true) {
-                    continue;
-                // ... and if is not allocated then we set the PCB's base and limit registers ...
-                } else if (this.segMap[i] === false) {
-                    this.segMap[i] = true;
-                    pcb.baseReg = i;
-                    pcb.limitReg = pcb.baseReg + 0xFF;
-                    break;
-                // ... otherwise they're all allocated and we exit the function before anything is written.
+                if (this.segMap[i] === false && program.length <= 0xFF) {
+                    this.currSeg = i;
+                    return true;
                 } else if (this.segMap[i] === 'allAllocated') {
                     return false;
                 }
             }
-            
+        }
+
+        public allocateMem(pcb: TSOS.ProcessControlBlock, program: number[]): void{
+            this.segMap[this.currSeg] = true; 
+            pcb.baseReg = this.currSeg;
+            pcb.limitReg = pcb.baseReg + 0xFF;
             // Now we actually write the program to memory
             for (let i = 0; i < program.length; i++) {
                 _MemoryAccessor.writeMem(pcb, i, program[i]);
             }
-            return true;
         }
 
+        // Clears a segment of memory associated with a PCB
+        public clearMemSeg(pcb: TSOS.ProcessControlBlock): void {
+            for (let i = pcb.baseReg; i < pcb.limitReg; i++) {
+                _MemoryAccessor.writeMem(pcb, i, 0x00);
+            }
+
+            // Marks segment as unallocated
+            this.segMap[pcb.baseReg] = false;
+            Devices.hostUpdateMemDisplay();
+        }
+
+        // Clears all of the memory, and their associated PCBs by clearing each segment
         public clearMem(): void {
-            // Create a temp PCB to use the MA, then we undo the currentPID increment so it is written over when we actually create another PCB
-            let tempPcb = new ProcessControlBlock();
-            tempPcb.createPCB();
-            TSOS.ProcessControlBlock.currentPID -= 1;
-            tempPcb.limitReg = 0x301;
-            for (let i = 0; i < 0x300; i++) {
-                _MemoryAccessor.writeMem(tempPcb, i, 0x000);
+            for (let i in this.pcbArr) {
+                this.clearMemSeg(this.pcbArr[i]);
             }
             Devices.hostUpdateMemDisplay();
         }
