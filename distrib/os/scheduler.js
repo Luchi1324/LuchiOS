6d4fcf3a2f443e3d9d9400cb24b11765ac78a28b
Luchi1324 <luchileigh8@gmail.com>
2023-11-04 18:27:35 -0400

    Commands + unnecessary logic

--- distrib/os/scheduler.js
+++ distrib/os/scheduler.js
@@ -8,16 +8,19 @@ var TSOS;
         constructor() {
             this.readyQueue = new TSOS.Queue();
             this.quantum = 6;
-            this.quantaCount = 0;
+            this.quantaCount = 1;
             this.executingPCB = null;
         }
         scheduleRR() {
+            // If we don't have a process executing and it is in the ready queue ...
             if (this.executingPCB === null && this.readyQueue.getSize() > 0) {
+                // ... then we load it into the CPU
                 this.executingPCB = this.readyQueue.dequeue();
                 _CPU.loadProgram(this.executingPCB);
             }
             else if (this.readyQueue.getSize() > 0) {
-                if ((this.quantaCount + 1) % this.quantum === 0) {
+                // Otherwise if we have reached our Quantum ...
+                if (this.quantaCount % this.quantum === 0) {
                     _Kernel.krnInterruptHandler(CONTEXT_SWITCH_IRQ, 0);
                 }
             }
