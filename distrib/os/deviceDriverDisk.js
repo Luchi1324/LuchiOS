ba9e23c2f00ddab46a8a1ebc75e11521326d29ff
Luchi1324 <luchileigh8@gmail.com>
2023-11-27 14:57:51 -0500

    Making start on finding a block for read, #21

--- distrib/os/deviceDriverDisk.js
+++ distrib/os/deviceDriverDisk.js
@@ -34,6 +34,18 @@ var TSOS;
         }
         readFile(fileName) {
         }
+        readBlockData(data) {
+            // split the data into an array of hex pairings (regex: every 2 characters)
+            let hexArr = data.match(/.{1,2}/g);
+            let block = '';
+            let i = 0;
+            // loop through array and build block string
+            while (i < hexArr.length) {
+                block += hexArr[i];
+                i++;
+            }
+            return block;
+        }
         writeFile(fileName) {
         }
         deleteFile(fileName) {
@@ -43,6 +55,55 @@ var TSOS;
         renameFile(fileName, newFileName) {
         }
         getAllFiles() {
+        }
+        findFile(fileName) {
+            let startingBlockKey = null;
+            let fileArr = [];
+            directorySearch: for (let t = 0; t < 1; t++) {
+                for (let s = 0; s < _Disk.numSectors; s++) {
+                    for (let b = 0; b < _Disk.numBlocks; b++) {
+                        let potentialKey = this.createStorageKey(t, s, b);
+                        let dataArr = sessionStorage.getItem(potentialKey).split(":");
+                        if (dataArr) {
+                            let metaData = dataArr[0];
+                            let fileData = this.trimData(dataArr[1]);
+                            let isUsed = this.checkIfInUse(metaData);
+                            if (isUsed && this.readBlockData(fileData) == (TSOS.Utils.txtToHex(fileName))) {
+                                startingBlockKey = metaData.slice(1, 4);
+                                // directory key
+                                fileArr.push(potentialKey);
+                                // starting block key
+                                fileArr.push(metaData.slice(1, 4));
+                                break directorySearch;
+                            }
+                        }
+                    }
+                }
+            }
+            return fileArr;
+        }
+        trimData(data) {
+            let hexArr = data.match(/.{1,2}/g);
+            let i = 0;
+            let res = '';
+            while (i < hexArr.length) {
+                if (hexArr[i] != '00') {
+                    res += hexArr[i];
+                }
+                else {
+                    break;
+                }
+                i++;
+            }
+            return res;
+        }
+        checkIfInUse(data) {
+            let isUsed = false;
+            let dataArr = data.split("");
+            if (dataArr[0] === "1") {
+                isUsed = true;
+            }
+            return isUsed;
         }
     }
     TSOS.DeviceDriverDisk = DeviceDriverDisk;
--- source/os/deviceDriverDisk.ts
+++ source/os/deviceDriverDisk.ts
@@ -36,11 +36,23 @@ module TSOS {
 
         public createFile(fileName: string) {
             let createdFlag: boolean = false;
-
         }
 
         public readFile(fileName: string) {
 
+        }
+
+        public readBlockData(data: string): string {
+            // split the data into an array of hex pairings (regex: every 2 characters)
+            let hexArr = data.match(/.{1,2}/g);
+            let block = '';
+            let i = 0;
+            // loop through array and build block string
+            while (i < hexArr.length) {
+                block += hexArr[i];
+                i++;
+            }
+            return block;
         }
         
         public writeFile(fileName: string) {
@@ -61,6 +73,61 @@ module TSOS {
 
         public getAllFiles() {
 
+        }
+
+        public findFile(fileName): string[] {
+            let startingBlockKey = null;
+            let fileArr = [];
+
+            directorySearch:
+            for (let t = 0; t < 1; t++) {
+                for (let s = 0; s < _Disk.numSectors; s++) {
+                    for (let b = 0; b < _Disk.numBlocks; b++) {
+                        let potentialKey = this.createStorageKey(t, s, b);
+                        let dataArr = sessionStorage.getItem(potentialKey).split(":");
+
+                        if (dataArr) {
+                            let metaData = dataArr[0];
+                            let fileData = this.trimData(dataArr[1]);
+                            let isUsed = this.checkIfInUse(metaData);
+
+                            if (isUsed && this.readBlockData(fileData) == (Utils.txtToHex(fileName))) {
+                                startingBlockKey = metaData.slice(1,4);
+                                // directory key
+                                fileArr.push(potentialKey);
+                                // starting block key
+                                fileArr.push(metaData.slice(1,4));
+                                break directorySearch;
+                            }
+                        }
+                    }
+                }
+            }
+            return fileArr;
+        }
+
+        public trimData(data: string): string {
+            let hexArr = data.match(/.{1,2}/g);
+            let i = 0;
+            let res = ''
+            while (i < hexArr.length) {
+                if (hexArr[i] != '00') {
+                    res += hexArr[i];
+                } else {
+                    break;
+                }
+                i++;
+            }
+            return res;
+        }
+
+        public checkIfInUse(data: string): boolean {
+            let isUsed = false;
+            let dataArr = data.split("");
+            if (dataArr[0] === "1") {
+                isUsed = true;
+            }
+            return isUsed;
         }
     }
 }
