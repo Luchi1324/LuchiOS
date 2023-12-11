508d68e69dd8c2e2fd6d99e6537e0f1a2e5a7982
Luchi1324 <luchileigh8@gmail.com>
2023-12-10 20:05:09 -0500

    PCB needs to track where it's saved, #29

--- distrib/os/swapper.js
+++ distrib/os/swapper.js
@@ -3,6 +3,18 @@ var TSOS;
     class Swapper {
         constructor() {
         }
+        rollIn(pcb) {
+        }
+        rollOut(pcb) {
+            let rollOutData = "";
+            for (let i = 0; i < ((pcb.limitReg - pcb.baseReg) * 2); i += 2) {
+                rollOutData += (_MemoryAccessor.readMem(pcb, i)).toString(16) + " ";
+            }
+            rollOutData.trim();
+            _MemoryManager.clearMemSeg(pcb);
+            _krnDiskDriver.createSwapFile(pcb, rollOutData);
+            TSOS.Devices.hostUpdateDiskDisplay();
+        }
     }
     TSOS.Swapper = Swapper;
 })(TSOS || (TSOS = {}));
--- source/os/swapper.ts
+++ source/os/swapper.ts
@@ -1,7 +1,23 @@
 module TSOS {
     export class Swapper {
-        constructor() {
+        constructor() { 
+        }
+
+        public rollIn(pcb: ProcessControlBlock): void {
+
+        } 
+
+        public rollOut(pcb: ProcessControlBlock): void {
+            let rollOutData: string = "";
+            for (let i = 0; i < ((pcb.limitReg - pcb.baseReg) * 2); i += 2) {
+                rollOutData += (_MemoryAccessor.readMem(pcb, i)).toString(16) + " ";
+            }
+
+            rollOutData.trim()
+            _MemoryManager.clearMemSeg(pcb);
             
+            _krnDiskDriver.createSwapFile(pcb, rollOutData)
+            Devices.hostUpdateDiskDisplay();
         }
     }
 }
