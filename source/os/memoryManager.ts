e639502a8b51362afd01e46aa0af35a6979a20cf
Luchi1324 <luchileigh8@gmail.com>
2023-12-11 20:11:24 -0500

    clearMem didn't terminate the PCB + update pcbTable

--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -4,6 +4,7 @@ module TSOS {
         public residentTasks: ProcessControlBlock[];
         public segMap: Object;
         public currSeg: number;
+        public memFull: boolean;
 
         constructor() {
             this.residentTasks = [];
@@ -12,8 +13,8 @@ module TSOS {
                 0x000: false,
                 0x100: false,
                 0x200: false,
-                0x300: 'allAllocated'
             };
+            this.memFull = false;
         }
 
         public loadMem(program: number[], pcbSwap?: ProcessControlBlock): boolean {
@@ -49,6 +50,8 @@ module TSOS {
                     this.allocateMem(pcbSwap, program);
                     pcbSwap.location = "Memory";
                     Devices.hostUpdatePcbDisplay(pcbSwap);
+                } else {
+                    alert("Load from swap failed");
                 }
             }
         }
@@ -57,12 +60,19 @@ module TSOS {
             // First we check the beginning of each segment ...
             for (let i = 0x000; i <= 0x300; i += 0x100) {
                 // ... if it is free and the program fits, it can be allocated ...
+                alert(`I: ${i}, SegMap: ${this.segMap[i]}, ProgLength: ${program.length}`);
                 if (this.segMap[i] === false && program.length <= 0xFF) {
+                    alert('Seg found!');
                     // Update the current segment that is being allocated
                     this.currSeg = i;
                     return true;
+                // ... or we haven't reached the end yet ...
+                } else if (i !== 0x300) {
+                    continue;
                 // ... or else they are all allocated then we can't allocate anymore.
-                } else if (this.segMap[i] === 'allAllocated') {
+                } else {
+                    this.memFull = true;
+                    alert('Memory is full')
                     return false;
                 }
             }
@@ -81,16 +91,20 @@ module TSOS {
         }
 
         // Clears a segment of memory associated with a PCB
-        public clearMemSeg(pcb: TSOS.ProcessControlBlock, swap?: boolean): void {
+        public clearMemSeg(pcb: TSOS.ProcessControlBlock, swap: boolean): void {
             for (let i = 0x00; i < 0xFF; i++) {
                 _MemoryAccessor.writeMem(pcb, i, 0x00);
             }
 
-            // Marks segment as unallocated, and removes from resident list (if not called from the swapper)
+            // Marks segment as unallocated, and sets the currSeg pointer to the cleared segment
             this.segMap[pcb.baseReg] = false;
 
-            if (!swap) {
+            // ... and removes from resident list (if not called from the swapper)
+            if (swap === false) {
+                alert('Removing from resident tasks')
+                pcb.terminatePCB();
                 _MemoryManager.residentTasks.splice(pcb.pid % 3, 1, null);
+                Devices.hostUpdatePcbDisplay(pcb);
             }
 
             Devices.hostUpdateMemDisplay();
@@ -99,7 +113,7 @@ module TSOS {
         // Clears all of the memory, and their associated PCBs by clearing each segment
         public clearMem(): void {
             for (let i in this.residentTasks) {
-                this.clearMemSeg(this.residentTasks[i]);
+                this.clearMemSeg(this.residentTasks[i], false);
             }
             Devices.hostUpdateMemDisplay();
         }
